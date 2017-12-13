f16532ea21813c9483e57d634557d0e0287f3e34
lixiangyang <489322130>
2017-12-13 12:36:02 +0800

    Initial commit

---
+++ src/main/java/com/java/lxyer/LxyerApplication.java
@@ -0,0 +1,12 @@
+package com.java.lxyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LxyerApplication.class, args);
+	}
+}
